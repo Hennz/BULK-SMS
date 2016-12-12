6aaaf86cf780113b9b2ee07661eeb66e03db32d8
safayatzisan <zisan_cse_mbstu@hotmail.com>
2016-12-12 12:31:18 +0600

    Add project files.

---
+++ BULK SMS/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace BULK_SMS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient client = new WebClient();
+                string to, message;
+                to = textBox1.Text;
+                message = textBox2.Text;
+                string baseURL = "http://api.clickatell.com/http/sendmsg?user=zisan94268&password=OYeNLVUHTNIHbD&api_id=3528011&to='" + to + "'&text='" + message + "'";
+                client.OpenRead(baseURL);
+                MessageBox.Show("Successfully sent message");
+            }
+            catch(Exception exp)
+            {
+                MessageBox.Show(exp.ToString());
+            }
+        }
+    }
+}
